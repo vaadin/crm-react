e6f2e7366fc7e00d13fdc2058aee129c0ef7ceb1
webking-dev <sunmandev@gmail.com>
2020-11-05 03:33:50 -0500

    fix: lint issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import type { FC } from 'react';
 import './App.css';
 
-const App: FC = () => {
-  return (
-    <div className="App-header">
-      <h1>Vaadin CRM</h1>
-    </div>
-  );
-};
+const App: FC = () => (
+  <div className="App-header">
+    <h1>Vaadin CRM</h1>
+  </div>
+);
 
 export default App;
