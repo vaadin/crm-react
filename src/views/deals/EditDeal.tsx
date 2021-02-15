e1a0a9a7fce92a2fcd6b8c216c3db33c27fac4c4
webking-dev <sunmandev@gmail.com>
2021-02-15 10:51:54 -0500

    feat: added quill for edit deal UI

--- src/views/deals/EditDeal.tsx
+++ src/views/deals/EditDeal.tsx
@@ -4,19 +4,22 @@ import {
   Drawer,
   makeStyles,
   Theme,
-  Grid,
   TextField,
   FormControl,
   Button,
   IconButton,
   InputLabel,
-  Select
+  Select,
+  Paper
 } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import LaunchIcon from '@material-ui/icons/Launch';
+import ReactQuill from 'react-quill';
 import { useSelector } from '../../store';
 import { State } from '../../reducers';
 import type { Deal } from '../../types';
+
+import 'react-quill/dist/quill.snow.css';
 
 const useStyles = makeStyles((theme: Theme) => ({
   paper: {
@@ -26,8 +29,8 @@ const useStyles = makeStyles((theme: Theme) => ({
     width: 400,
     padding: theme.spacing(2)
   },
-  flex1: {
-    flexGrow: 1
+  w100: {
+    width: '100%'
   },
   control: {
     display: 'flex',
@@ -37,11 +40,26 @@ const useStyles = makeStyles((theme: Theme) => ({
   hidden: {
     display: 'none'
   },
-  company_selector: {
+  detail_row: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    width: '100%'
+    width: '100%',
+    paddingBottom: theme.spacing(2)
+  },
+  note: {
+    flexDirection: 'column',
+    paddingBottom: theme.spacing(2)
+  },
+  no_button: {
+    width: 'calc(100% - 48px)'
+  },
+  temp: {
+    width: 'calc(100% - 48px)',
+    height: '50px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 }));
 
@@ -54,17 +72,46 @@ interface EditDealProps {
 const EditDeal: FC<EditDealProps> = ({ curDeal, isEdit, toggleDrawer }) => {
   const classes = useStyles();
   const [dealData, setDealData] = useState<any>(curDeal);
-  const { data } = useSelector((state: State) => state.companies);
+  const { companies, users } = useSelector((state: State) => ({
+    companies: state.companies,
+    users: state.users
+  }));
 
   useEffect(() => {
     setDealData(curDeal);
+    console.log(curDeal);
   }, [curDeal]);
 
   const handleChange = (e: React.ChangeEvent<any>) => {
-    setDealData({
-      ...dealData,
-      [e.target.name]: e.target.value
-    });
+    console.log(dealData);
+    if (e.target.name === 'dealPrice') {
+      const regex = new RegExp('^[0-9]*$');
+
+      if (regex.test(e.target.value)) {
+        e.preventDefault();
+      }
+    }
+
+    if (e.target.name === 'company' || e.target.name === 'user') {
+      setDealData((prev: any) => ({
+        ...prev,
+        [e.target.name]: {
+          id: e.target.value
+        }
+      }));
+    } else {
+      setDealData((prev: any) => ({
+        ...prev,
+        [e.target.name]: e.target.value
+      }));
+    }
+  };
+
+  const handleChangeNote = (value: any) => {
+    setDealData((prev: any) => ({
+      ...prev,
+      dealNote: value
+    }));
   };
 
   const handleDeleteDeal = () => {
@@ -90,10 +137,15 @@ const EditDeal: FC<EditDealProps> = ({ curDeal, isEdit, toggleDrawer }) => {
     >
       <div className={classes.pane}>
         <form onSubmit={handleSubmit}>
-          <Grid container justify="space-between">
+          <FormControl
+            variant="outlined"
+            size="small"
+            required
+            className={classes.detail_row}
+          >
             <TextField
-              name="dealName"
-              className={classes.flex1}
+              name="name"
+              className={classes.w100}
               label="Deal Name"
               value={dealData?.name || ''}
               onChange={handleChange}
@@ -104,15 +156,15 @@ const EditDeal: FC<EditDealProps> = ({ curDeal, isEdit, toggleDrawer }) => {
             <IconButton aria-label="delete" onClick={handleDeleteDeal}>
               <DeleteIcon />
             </IconButton>
-          </Grid>
+          </FormControl>
 
           <FormControl
             variant="outlined"
             size="small"
             required
-            className={classes.company_selector}
+            className={classes.detail_row}
           >
-            <div className={classes.flex1}>
+            <div className={classes.w100}>
               <InputLabel htmlFor="outlined-company">Company</InputLabel>
               <Select
                 label="Company"
@@ -126,7 +178,7 @@ const EditDeal: FC<EditDealProps> = ({ curDeal, isEdit, toggleDrawer }) => {
                 }}
               >
                 <option value="" className={classes.hidden} />
-                {data?.map((company) => {
+                {companies.data?.map((company) => {
                   return (
                     <option key={company.id} value={company.id}>
                       {company.name}
@@ -138,6 +190,69 @@ const EditDeal: FC<EditDealProps> = ({ curDeal, isEdit, toggleDrawer }) => {
             <IconButton aria-label="openCompany">
               <LaunchIcon />
             </IconButton>
+          </FormControl>
+
+          <FormControl className={classes.detail_row}>
+            <Paper className={classes.temp}>
+              Here is the contacts table area
+            </Paper>
+            <IconButton aria-label="openCompany">
+              <LaunchIcon />
+            </IconButton>
+          </FormControl>
+
+          <FormControl className={classes.detail_row}>
+            <TextField
+              name="price"
+              label="Deal Price"
+              className={classes.no_button}
+              value={dealData?.price || ''}
+              type="number"
+              onChange={handleChange}
+              variant="outlined"
+              size="small"
+              required
+            />
+          </FormControl>
+
+          <FormControl
+            variant="outlined"
+            size="small"
+            className={classes.detail_row}
+            required
+          >
+            <InputLabel htmlFor="outlined-user">Manager</InputLabel>
+            <Select
+              label="Manager"
+              value={dealData?.user?.id}
+              className={classes.no_button}
+              onChange={handleChange}
+              native
+              inputProps={{
+                name: 'user',
+                id: 'outlined-user'
+              }}
+            >
+              <option value="" className={classes.hidden} />
+              {users.data?.map((user) => {
+                return (
+                  <option key={user.id} value={user.id}>
+                    {user.name}
+                  </option>
+                );
+              })}
+            </Select>
+          </FormControl>
+
+          <FormControl className={classes.detail_row} required>
+            <ReactQuill
+              className={classes.w100}
+              value={dealData?.dealNote || ''}
+              onChange={handleChangeNote}
+            />
+            <Button variant="contained" fullWidth>
+              Add note
+            </Button>
           </FormControl>
 
           <FormControl className={classes.control}>
